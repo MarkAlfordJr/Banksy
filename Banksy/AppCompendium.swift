63a661146e1933eb49f7118280ce99f28df1d5b2
Mark Alford Jr <markalfordjr@gmail.com>
2025-06-02 20:22:51 -0400

    Initial Login Feature Start

---
+++ Banksy/AppCompendium.swift
@@ -0,0 +1,24 @@
+//
+//  AppCompendium.swift
+//  Banksy
+//
+//  Created by Mark Alford on 6/2/25.
+//
+
+/*
+ REMOVE STORYBOARDS
+ 1. remove neccessary files
+ 2. update info.plist
+ 3. update APP delegate
+ 
+ 1. delete SceneDelegate file
+ 2. delete storyboard file
+ 3. in info.plist, delete the 'Application Scene Manifest'
+ 
+ - Update App Delegate
+ */
+
+/*
+ Building Views Programatically
+ - start with Custom UIView
+ */
